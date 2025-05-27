141806bb9851b65f46ab3d3f901eb03e42070058
Aasritha-Gopisetti <ashrithagopisetti@gmail.com>
2025-05-27 16:45:44 +0530

    Leaves are added in admin

--- corevolthrm/admin.py
+++ corevolthrm/admin.py
@@ -2,3 +2,14 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
 admin.site.register(CustomUser)
+admin.site.register(CustomUser)
+from .models import LeaveApplication
+
+@admin.register(LeaveApplication)
+class LeaveApplicationAdmin(admin.ModelAdmin):
+    list_display = ('get_user_fullname', 'leaveType', 'startDate', 'endDate', 'status')
+    def get_user_fullname(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+    get_user_fullname.short_description = 'Employee Name'
+    list_filter = ('leaveType', 'status')
+    search_fields = ('employeeId', 'reason')
